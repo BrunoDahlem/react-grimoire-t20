990187ea89775bde7c65b26e14b2d44aca17ede9
Bruno Dahlem <brunodahlempereira.1306@gmail.com>
2023-07-07 17:04:55 -0300

    Api forms reweb

--- src/pages/api/forms/index.ts
+++ src/pages/api/forms/index.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { data } from '../../../api/forms';
+import { forms } from '../../../api/forms';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(data);
+  res.status(200).json(forms);
 }
