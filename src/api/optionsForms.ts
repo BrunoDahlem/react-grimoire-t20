d85ab1c1f687b32678affe160a828914f462b3ef
Bruno Dahlem <brunodahlempereira.1306@gmail.com>
2023-07-20 16:04:47 -0300

    Adidionada api forms reweb para teste

--- src/api/optionsForms.ts
+++ src/api/optionsForms.ts
@@ -1,8 +1,63 @@
 export const options = {
   options: [
     {
-      value: '',
-      label: 'unique-label',
+      value: 44,
+      label: 'Consórcio',
+    },
+
+    {
+      value: 1,
+      label: 'Banner',
+    },
+
+    {
+      value: 17,
+      label: 'Detalhe do produto',
+    },
+
+    {
+      value: 3,
+      label: 'Detalhe do veículo novo',
+    },
+
+    {
+      value: 54,
+      label: 'Detalhe do veículo seminovo',
+    },
+
+    {
+      value: 41,
+      label: 'Noticias - Detalhe',
+    },
+
+    {
+      value: 8,
+      label: 'Formulário whatsapp',
+    },
+
+    {
+      value: 50,
+      label: 'Seguros',
+    },
+
+    {
+      value: 37,
+      label: 'Serviço',
+    },
+
+    {
+      value: 39,
+      label: 'Venda Direta',
+    },
+
+    {
+      value: 91,
+      label: 'Serviço',
+    },
+
+    {
+      value: 8,
+      label: 'Formulário whatsapp',
     },
   ],
 };
--- src/pages/api/forms/index.ts
+++ src/pages/api/forms/index.ts
@@ -1,12 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 //import { forms } from '../../../api/forms';
-import { fetchDataForms } from '../../../api/fetchData';
+//import { fetchDataForms } from '../../../api/fetchData';
+import { options } from '../../../api/optionsForms';
 
 // export default function handler(req: NextApiRequest, res: NextApiResponse) {
 //   res.status(200).json(forms);
 // }
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json(await fetchDataForms());
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json(options);
 };
 export default handler;
